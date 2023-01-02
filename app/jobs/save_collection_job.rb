be996461ac3157ca6d631e2d01aa0c760c1f8a6b
Maxence Robinet <maxencerobinet@gmail.com>
2023-01-02 14:29:51 +0100

    fix save job

--- app/jobs/save_collection_job.rb
+++ app/jobs/save_collection_job.rb
@@ -4,16 +4,18 @@ class SaveCollectionJob < ApplicationJob
   def perform(releases, user_id)
     puts '****** STARTS JOB *******'
     # retrieves user with user_id
-    user = User.find(user_id)
+    current_user = User.find(user_id)
     # For each release of releases parameter...
     releases.each do |release|
       # Create new reference in DB
-      Release.create(id: release["id"], user: user, data: release)
+      Release.find_or_create_by(id: release["id"], user: current_user) do |item|
+        item.data = release
+      end
     end
     # collection? => true
-    user.save_collection!
+    current_user.save_collection!
     puts '****** JOB DONE *******'
-    puts "#{user.username} collection saved? == #{user.collection?}"
+    puts "#{current_user.username} collection saved? == #{current_user.collection?}"
     puts '****** SEE YOU LATER *******'
   end
 end
