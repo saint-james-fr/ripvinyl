3669186782382f8e28674ae96b80803e1afe554e
Maxence Robinet <maxencerobinet@gmail.com>
2023-01-10 17:10:12 +0100

    fixed registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   include Token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
   has_many :releases
 
