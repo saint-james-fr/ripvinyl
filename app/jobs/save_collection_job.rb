59bf10e0f9f33d7c4608a65bf46be380254a9fa7
Maxence Robinet <maxencerobinet@gmail.com>
2023-09-06 22:25:27 +0200

    improving save colection job 3

--- app/jobs/save_collection_job.rb
+++ app/jobs/save_collection_job.rb
@@ -3,38 +3,31 @@ class SaveCollectionJob < ApplicationJob
 
   def perform(releases, user_id)
     puts '****** STARTS JOB *******'
-    
+
     begin
       # Retrieves user with user_id
       current_user = User.find(user_id)
 
-      # Batch processing and bulk insert
-      releases.each_slice(100) do |batch_releases|
-        Release.insert_all(batch_releases.map { |release| { id: release["id"], user_id: current_user.id, data: release } })
-      end
-      
-      # Use a transaction for data consistency
       ActiveRecord::Base.transaction do
-        releases.each do |release|
-          Release.find_or_create_by(id: release["id"], user_id: current_user.id) do |item|
-            item.data = release
-          end
+        # Batch processing and bulk insert
+        releases.each_slice(100) do |batch_releases|
+          Release.insert_all(batch_releases.map { |release| { id: release["id"], user_id: current_user.id, data: release } })
         end
+
+        # Collection? => true
+        current_user.save_collection!
       end
-      
-      # Collection? => true
-      current_user.save_collection!
-      
+
       puts '****** JOB DONE *******'
       puts "#{current_user.username} collection saved? == #{current_user.collection?}"
       puts '****** SEE YOU LATER *******'
-      
+
     rescue StandardError => e
       # Handle any errors that occur during the job execution
       puts '****** ERROR OCCURRED *******'
       puts "Error message: #{e.message}"
       puts '****** SEE YOU LATER *******'
-      
+
       # You may also want to log the error or notify administrators here.
     end
   end
